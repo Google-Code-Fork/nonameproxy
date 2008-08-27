9f8fba1f86adff017342e6a2c20f8330dbe45f88
carlchatfield <carlchatfield@552dbc66-3b53-0410-9e70-cb4f4a4d289b>
2008-08-27 13:23:01 +0000

    added TThing and TOutfit to tibiatypes. These are not yet complete, as they require a factory to be read and i am rethinking how tibia messages will be read. I am thinking about adding a peek function to network message, also discovered error in make file, but im too tired to fix it. If you have any linker errors make clean and recompile.

--- tests/dattest.cpp
+++ tests/dattest.cpp
@@ -4,6 +4,10 @@
 int main ()
 {
         DatReader* dat = new DatReader ();
+        for (int i = 0; i < dat->getNIds (); i ++) {
+                dat->getItemData (i + 100)->show ();
+        }
+        delete dat;
         return 0;
 }
 
