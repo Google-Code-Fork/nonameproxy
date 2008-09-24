b7ffb820934d6ac7ca01d36483386825093bfbab
carlchatfield <carlchatfield@552dbc66-3b53-0410-9e70-cb4f4a4d289b>
2008-09-24 08:38:16 +0000

    added map state, forgot to do this before so last 2 revisions will not compile

---
+++ mapstate.h
@@ -0,0 +1,27 @@
+#ifndef __MAP_STATE_H
+#define __MAP_STATE_H
+
+#include <string>
+#include <stdint.h>
+
+class Pos
+{
+        public:
+                Pos ();
+                Pos (uint32_t xx, uint32_t yy, uint32_t zz);
+
+                uint32_t x;
+                uint32_t y;
+                uint32_t z;
+};
+
+class MapState
+{
+        public:
+                Pos  getCurPos ();
+                void setCurPos (const Pos& pos);
+        private:
+                Pos _curPos;
+};
+#endif
+
