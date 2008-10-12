d3bfa2f3cab469db44df89de5a7d8eac16617036
carlchatfield <carlchatfield@552dbc66-3b53-0410-9e70-cb4f4a4d289b>
2008-10-12 08:27:05 +0000

    added channelmanager

---
+++ trunk/plugins/channelmanager/channelmanager.cpp
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include "channelmanager.h"
+#include "enums.h"
+
+ChannelManager chanMgr;
+
+TibiaMessage* ChannelListHook::func (TibiaMessage* tm, Client* client)
+{
+        return chanMgr.hookChannelList ((GRMChannelList*)tm, client);
+}
+
+TibiaMessage* ChannelOpenHook::func (TibiaMessage* tm, Client* client)
+{
+        return chanMgr.hookChannelOpen ((GSMChannelOpen*)tm, client);
+}
+
+TibiaMessage* ChannelCloseHook::func (TibiaMessage* tm, Client* client)
+{
+        return chanMgr.hookChannelClose ((GSMChannelClose*)tm, client);
+}
+
+TibiaMessage* SpeakHook::func (TibiaMessage* tm, Client* client)
+{
+        return chanMgr.hookSpeak ((GSMSpeak*)tm, client);
+}
+
+void ChannelRecipricant::func (const Args& args)
+{
+        Args::const_iterator i = args.begin ();
+        if (args.size () < 2) {
+                return;
+        }
+        if ((*i) != "channelmanager") {
+                return;
+        }
+        i ++;
+        if ((*i) == "add") {
+                if (args.size () != 4) {
+                        printf ("channel manager: add takes 4 args\n");
+                        return;
+                }
+                i ++;
+                const std::string& name = *i;
+                i ++;
+                uint32_t id = atoi ((*i).c_str ());
+                if (id > 0xFFFF) {
+                        printf ("warning channel id out of range\n");
+                        id %= 0xFFFF;
+                }
+                chanMgr.addChannel (name, id);
+        } else if ((*i) == "remove") {
+                if (args.size () != 3) {
+                        printf ("channel manager: remove takes 3 args\n");
+                        return;
+                }
+                i ++;
+                uint32_t id = atoi ((*i).c_str ());
+                if (id > 0xFFFF) {
+                        printf ("warning channel id out of range\n");
+                        id %= 0xFFFF;
+                }
+                chanMgr.removeChannel (id);
+        } else if ((*i) == "blacklist") {
+                if (args.size () != 3) {
+                        printf ("channel manager: blacklist takes 3 args\n");
+                        return;
+                }
+                i ++;
+                uint32_t id = atoi ((*i).c_str ());
+                if (id > 0xFFFF) {
+                        printf ("warning channel id out of range\n");
+                        id %= 0xFFFF;
+                }
+                chanMgr.blackListChannel (id);
+        } else if ((*i) == "whitelist") {
+                if (args.size () != 3) {
+                        printf ("channel manager: whitelist takes 4 args\n");
+                        return;
+                }
+                i ++;
+                uint32_t id = atoi ((*i).c_str ());
+                if (id > 0xFFFF) {
+                        printf ("warning channel id out of range\n");
+                        id %= 0xFFFF;
+                }
+                chanMgr.whiteListChannel (id);
+        } else {
+                printf ("channel manager: unreconized command %s\n", (*i).c_str ());
+        }
+}
+
+void ChannelManager::addChannel (const std::string& name, uint32_t id)
+{
+        printf ("adding\n");
+        if (addList.count (id) != 0) {
+                printf ("channelmanager add error: channel already added\n");
+                return;
+        }
+        addList.insert (std::pair<uint32_t, std::string> (id, name));
+}
+
+void ChannelManager::removeChannel (uint32_t id)
+{
+        if (addList.erase (id) == 0) {
+                printf ("channelmanager remove error: channel not added\n");
+        }
+}
+
+void ChannelManager::blackListChannel (uint32_t id)
+{
+        if (addList.count (id) != 0) {
+                printf ("channelmanager blacklist error: may not blacklist ");
+                printf ("channels added by the manager\n");
+                return;
+        }
+        std::pair<ChannelSet::iterator, bool> ret = blackList.insert (id);
+        if (ret.second == false) {
+                printf ("channelmanager blacklist error: channel already ");
+                printf ("blacklisted\n");
+        }
+}
+
+void ChannelManager::whiteListChannel (uint32_t id)
+{
+        if (blackList.erase (id) == 0) {
+                printf ("channelmanager whitelist error: channel not blacklisted\n");
+        }
+}
+
+GSMChannelOpen* ChannelManager::hookChannelOpen (GSMChannelOpen* co, Client* client)
+{
+        uint32_t channelId = co->getChannelId ();
+        if (addList.count (channelId) != 0) {
+                printf ("blocked\n");
+                delete co;
+                return NULL;
+        } else {
+                printf ("whoops\n");
+                return co;
+        }
+}
+
+GRMChannelList* ChannelManager::hookChannelList (GRMChannelList* cl, Client* client)
+{
+        /* copy the old channel list */
+        TChannelList* channels = new TChannelList (cl->getChannelList ());
+        channels->begin ();
+        while (!channels->isEnd ()) {
+                uint32_t channelId = channels->getChannel ().getChannelId ();
+                if (blackList.count (channelId) != 0) {
+                        channels->remove ();
+                }
+                channels->next ();
+        }
+        ChannelMap::iterator i;
+        for (i = addList.begin (); i != addList.end (); i ++) {
+                channels->add (new TChannel ((*i).first, (*i).second));
+        }
+        delete cl;
+        return new GRMChannelList (channels);
+}
+
+void ChannelManager::iload (uint32_t pluginId, Client* client)
+{
+        _name = "channelmanager";
+        _pluginId = pluginId;
+        _client = client;
+
+        _channellist_hid = _client->addRecvWriteHook (_pluginId, 
+                                GRM_CHANNEL_LIST_ID, new ChannelListHook ());
+
+        _channelopen_hid = _client->addSendWriteHook (_pluginId, 
+                                GSM_CHANNEL_OPEN_ID, new ChannelOpenHook ());
+
+        _rid = _client->addRecipricant (_pluginId, new ChannelRecipricant ());
+}
+
+void ChannelManager::iunload ()
+{
+        _client->deleteRecvWriteHook (_pluginId, _channellist_hid);
+        _client->deleteSendWriteHook (_pluginId, _channelopen_hid);
+        _client->deleteRecipricant (_pluginId, _rid);
+}
+
+const std::string& ChannelManager::iname ()
+{
+        return _name;
+}
+
+void load (uint32_t id, Client* client)
+{
+        chanMgr.iload (id, client);
+}
+
+void unload ()
+{
+        chanMgr.iunload ();
+}
+
+const std::string& name ()
+{
+        return chanMgr.iname ();
+}
+
---
+++ trunk/plugins/channelmanager/channelmanager.h
@@ -0,0 +1,108 @@
+#ifndef __CHANNEL_MANAGER_H
+#define __CHANNEL_MANAGER_H
+
+#include <string>
+#include <set>
+#include <map>
+#include <stdint.h>
+
+#include "tibiamessage.h"
+#include "hook.h"
+#include "client.h"
+
+/**************************************************************************
+ * channel contains a  recv write hook which waits for the List Channels 
+ * Message. When this message is recieved, it will alter it according to a 
+ * add list of channels to add, and a black list of channels to block. 
+ *      channelmanager add channelname channelid
+ *      channelmanager remove channelid
+ *      channelmanager blacklist channelid
+ *      channelmanager whitelist channelid
+ * After a channel has been added, several other write hooks blocks all
+ * messages sent to the server which use the added channel id.
+ * These Include:
+ *      GSMChannelOpen
+ *      GSMChannelClose
+ *      GSMSpeak
+ * Remember that a read hook will still be able to respond to these 
+ * messages
+ *
+ * Note: Write hooks are placed on
+ * Recv:
+ *      GRMChannelList
+ * Send:
+ *      GSMChannelOpen
+ *      GSMChannelClose
+ *      GSMSpeak
+ **************************************************************************/
+
+class ChannelListHook : public WriteHook
+{
+        virtual TibiaMessage* func (TibiaMessage* tm, Client* client);
+};
+
+class ChannelOpenHook : public WriteHook
+{
+        virtual TibiaMessage* func (TibiaMessage* tm, Client* client);
+};
+
+class ChannelCloseHook : public WriteHook
+{
+        virtual TibiaMessage* func (TibiaMessage* tm, Client* client);
+};
+
+class SpeakHook : public WriteHook
+{
+        virtual TibiaMessage* func (TibiaMessage* tm, Client* client);
+};
+
+class ChannelRecipricant : public Recipricant
+{
+        virtual void func (const Args& args);
+};
+
+typedef std::map<uint32_t, std::string> ChannelMap;
+typedef std::set<uint32_t> ChannelSet;
+
+class ChannelManager
+{
+        public:
+                void addChannel (const std::string& name, uint32_t id);
+                void removeChannel (uint32_t id);
+
+                void blackListChannel (uint32_t id);
+                void whiteListChannel (uint32_t id);
+
+                GRMChannelList*  hookChannelList (GRMChannelList* cl, Client* client);
+
+                GSMChannelOpen*  hookChannelOpen (GSMChannelOpen* co, Client* client);
+                GSMChannelClose* hookChannelOpen (GSMChannelClose* cc, Client* client);
+                GSMSpeak*        hookChannelOpen (GSMSpeak* co, Client* client);
+
+                void iload (uint32_t pluginId, Client* client);
+                void iunload ();
+                const std::string& iname ();
+
+        private:
+                uint32_t _pluginId;
+                uint32_t _channellist_hid;
+                uint32_t _channelopen_hid;
+                uint32_t _channelclose_hid;
+                uint32_t _speak_hid;
+                uint32_t _rid;
+                Client*  _client;
+                std::string _name;
+
+                ChannelMap addList;
+                ChannelSet blackList;
+};
+
+extern "C"
+{
+        void load (uint32_t id, Client* client);
+        void unload ();
+        const std::string& name ();
+}
+
+#endif
+
